0f0181e36ccb39fd0fd4063a14b4c1eff3068eeb
unknown <amirzanganeh1999@gmail.com>
2022-09-05 13:03:00 +0430

    finish

--- src/Components/shared/UserCard.js
+++ src/Components/shared/UserCard.js
@@ -1,52 +1,65 @@
 import React from "react";
 import styles from "../../styles/user.module.css";
 import avatar from "../../assets/avatar/avatar.png";
-
+import GroupsIcon from "@mui/icons-material/Groups";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import AllInclusiveIcon from "@mui/icons-material/AllInclusive";
+import LocalCafeIcon from '@mui/icons-material/LocalCafe';
 export default function UserCard({ usersData }) {
-  const { id,name, information, points, level, awards, matches, pals } = usersData;
+  const { id, name, information, points, level, awards, matches, pals } =
+    usersData;
   return (
-    <div className={id== "1" ? styles.userRed : styles.userGreen}>
+      <div className={styles.container}>
+    <div className={id == "1" ? styles.userRed : styles.userGreen}>
       <div className={styles.userProfile}>
         <span className={styles.level}>level {level}</span>
-        <img src={avatar} alt="avatar" className={styles.avatar} />
+        <img src={avatar} title="Teacher" alt="avatar" className={styles.avatar} />
         <span className={styles.points}>{points} points</span>
       </div>
       <div className={styles.information}>
-        <p className={styles.name}>{name}</p>
+        <h1 className={styles.name}>{name}</h1>
         <div className={styles.details}>
-          <div>
+          <div className={styles.detailsGroup}>
             <p>Group Name</p>
+            <p>Joined January 2019</p>
+          </div>
+          <div className={styles.detailsGroup}>
+            <p>Position/Role</p>
             <p>City,Coutry</p>
           </div>
-          <div>
-            <p>Joined January 2019</p>
-            <p>Position/Role</p>
-          </div>
-
         </div>
-        <div className={styles.awards}>
-            <div>
-                <span>awards</span>
-              
-                <span>{awards}</span>
-            </div>
-            <div>
-                <span>matches</span>
-                <img src="" alt="" />
-                <span>{matches}</span>
-            </div>
-            <div>
-                <span>pals</span>
-                <img src="" alt="" />
-                <span>{pals}</span>
-            </div>
-            <div>
-                <span>coffee</span>
-                <img src="" alt="" />
-                <span></span>
-            </div>
+        <div className={styles.stats}>
+          <div>
+            <span className={styles.stateTitle}>awards</span>
+            <EmojiEventsIcon fontSize="large"/>
+            <span className={styles.number}>{awards}</span>
+          </div>
+          <div>
+            <span className={styles.stateTitle}>matches</span>
+            <SportsEsportsIcon fontSize="large"/>
+            <span className={styles.number}>{matches}</span>
+          </div>
+          <div>
+            <span className={styles.stateTitle}>pals</span>
+            <GroupsIcon fontSize="large"/>
+
+            <span className={styles.number}>{pals}</span>
+          </div>
+          <div>
+            <span className={styles.stateTitle}>coffee</span>
+            <LocalCafeIcon fontSize="large"/>
+            <AllInclusiveIcon fontSize="medium"/>
+          </div>
         </div>
       </div>
+
     </div>
+      <div className={styles.desc}>
+          <h1 className={styles.name}>{name}</h1>
+          <p>{information}</p>
+          <span className={styles.more}>Mouse over the card for more info</span>
+      </div>
+      </div>
   );
 }
